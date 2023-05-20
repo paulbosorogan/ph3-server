76ef94ff6f52a36052f9849ec389c0c0cd7e5a28
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-05-20 10:44:38 -0400

    Add routes GET, PATCH

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,50 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+    #GET
+    get "/" do
+      { message: "Good luck with your project!" }.to_json
+    end
 
+    get '/books' do 
+      books = Book.all.order(:title).limit(10)
+      books.to_json
+    end
+
+    get '/books/:id' do 
+      book = Book.find(params[:id])
+      book.to_json( include: { reviews: { include: :user}})
+    end
+
+    get '/reviews' do 
+      reviews = Review.all
+      reviews.to_json
+    end 
+
+    get '/reviews/best_score' do 
+      best_score = Review.best_score
+      best_score.to_json
+    end
+
+    #POST 
+    post '/reviews' do 
+      new_review = Review.create(
+        score: params[:score],
+        comment: params[:comment],
+        book_id: params[:book_id]
+      )
+      new_review.to_json
+    end 
+
+    #PATCH 
+    patch 'books/:id' do 
+      update_book = Book.find(params[:id])
+      update_book.update(
+        title: params[:title],
+        author: params[:author],
+        genre: params[:genre],
+        description: params[:description]
+      )
+      update_book.to_json
+    end 
 end
