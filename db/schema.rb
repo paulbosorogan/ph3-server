5ba26c1a95bf3b6c4c183fee7ac1356629c5b505
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-05-26 17:54:11 -0400

    Update due to new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_20_144125) do
+ActiveRecord::Schema.define(version: 2023_05_20_144110) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,23 +18,12 @@ ActiveRecord::Schema.define(version: 2023_05_20_144125) do
     t.string "genre"
     t.string "description"
     t.string "image_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "score"
     t.string "comment"
     t.integer "book_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
