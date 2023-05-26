a3a5db472c830913c2f7de78349735f59a39ad2e
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-05-26 17:54:53 -0400

    Update models book.rb and review.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,3 @@
 class Book < ActiveRecord::Base 
-    has_many :reviews
-    has_many :users, through: :reviews
+    has_many :reviews, dependent: :destroy
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,3 @@
 class Review <ActiveRecord::Base
     belongs_to :book 
-    belongs_to :user
-
-    def self.best_score
-        self.all.order(reviews: :desc).first
-      end
 end
