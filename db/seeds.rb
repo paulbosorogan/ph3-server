e16b940d2a4ff4b8715b4f8bec4915a0f6b83e5a
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-05-26 17:53:53 -0400

    Eliminated user.rb model and seed information

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,14 @@ puts "🌱 Seeding spices..."
 # Seed your database here
 #dummy book covers 
 book_covers = [
-    "https://i.imgur.com/jtnrnfJ.jpg",
-    "https://i.imgur.com/nqWeXPp.jpg",
-    "https://i.imgur.com/LrhWgUx.jpg",
-    "https://i.imgur.com/PV4lEKr.jpg",
-    "https://i.imgur.com/BGfqRiK.jpg"
+    "https://i.imgur.com/XokuxGV.jpg",
+    "https://i.imgur.com/7iRhvgP.jpg",
+    "https://i.imgur.com/z4DOmOK.jpg"
 ]
 
-#make 10 users
-10.times do 
-    User.create(name: Faker::Name.name)
-end
 
 #make 50 books
-50.times do 
+10.times do 
     book =Book.create(
         title: Faker::Book.title,
         author: Faker::Book.author,
@@ -25,14 +19,11 @@ end
         image_url: book_covers.sample)
 
         #create random reviews
-        rand(1..3).times do 
-            user = User.order('RANDOM()').first
-
+        1.times do 
             Review.create(
                 score: rand(1..10),
                 comment: Faker::Lorem.sentence,
                 book_id: book.id,
-                user_id: user.id
             )
     end
 end 
