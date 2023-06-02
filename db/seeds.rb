e9d41bdef81112551a9ac88e34d8fb7f847a0cfd
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-06-02 17:23:27 -0400

    Update Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,7 @@ puts "🌱 Seeding spices..."
 # Seed your database here
 #dummy book covers 
 book_covers = [
-    "https://i.imgur.com/XokuxGV.jpg",
-    "https://i.imgur.com/7iRhvgP.jpg",
+    "https://i.imgur.com/K7ySj95.jpg",
 ]
 
 
@@ -13,14 +12,7 @@ book_covers = [
     book =Book.create(
         title: Faker::Book.title,
         author: Faker::Book.author,
+        genre: Faker::Book.genre,
         image: book_covers.sample)
-
-        #create random reviews
-        1.times do 
-            Review.create(
-                comment: Faker::Lorem.sentence,
-                book_id: book.id,
-            )
-    end
 end 
 puts "✅ Done seeding!"
